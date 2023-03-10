8b88776d0f9e9959963866592d0aa3b3e34bd9f9
Amin <34860@ma-web.nl>
2023-03-10 22:06:14 +0100

    les 3 af

---
+++ prog les 3/Assets/character/MoveBasic.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MoveBasic : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] public float jumpPower = 3f;
+    [SerializeField] public float movementSpeed = 25f;
+    [SerializeField] public float rotSpeed = 125f;
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movementSpeed);
+        }
+        transform.Rotate(Vector3.down * -rotSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+}
